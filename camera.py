5db56486d3fb07e3471041182eee818cb05f2b06
anita-hu <anitahu113@gmail.com>
2018-05-05 17:00:38 -0400

    smile meter

--- camera.py
+++ camera.py
@@ -13,6 +13,8 @@ def Detect():
     counter2 = 0
     global facepts
     global smilepts
+    global meter
+    meter = 50
     while True:
         ret, image = VideoCamera().read()  # Capture frame-by-frame
         image = cv2.resize(image, (0, 0), fx=0.4, fy=0.4)
@@ -35,6 +37,7 @@ def Detect():
             roi_gray = gray[y:y+h, x:x+w]
 
             facepts = x, y, w, h
+            #meter +=10
 
             # Find smile
             smile = smileCascade.detectMultiScale(roi_gray, 1.7, 14)
@@ -44,23 +47,20 @@ def Detect():
                 counter2 = 0
                 a, b, c, d = facepts
                 smilepts = (x+a), (y+b), w, h
-                
-                #meter += 25
+                #meter += 10
 
         if counter1 == 2:
             facepts = (1,1,1,1)
         if counter2 == 2:
             smilepts = (1,1,1,1)
-            #meter -= 1
+            #meter -= 2
+        
 
 
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0 and read the first frame
         self.video = cv2.VideoCapture(0)
-        
-        #global meter
-        #meter = 5
         
         # start the face, smile recognition thread
         t = threading.Thread(target=Detect, name='thread1', args=())
@@ -82,22 +82,22 @@ class VideoCamera(object):
         try:
             # draw rectangles for face
             x, y, w, h = facepts  # get points from global variable
-            print(x,y,w,h)
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
             
             # draw rectangles for smile
             x2, y2, w2, h2 = smilepts
             cv2.rectangle(image, (x2, y2), (x2+w2, y2+h2), (255, 255, 255), 2)
 
-            
         except NameError:
             print('waiting')
-            
+       
+        cv2.rectangle(image, (20, 210), (meter+20, 220), (0, 255, 0), cv2.FILLED)
+
+             
         # smile meter
-        # font = ImageFont.truetype('Roboto-Regular.ttf',30)
+        #font = ImageFont.truetype('Roboto-Regular.ttf',30)
         #draw.text((5,300),'smile meter:', font=font)
         
-        #cv2.rectangle(image, (20, 300), (meter, 310), (0, 255, 0), cv2.FILLED)
 
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
